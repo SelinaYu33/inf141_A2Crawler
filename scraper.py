0219d69d729305500765cca3f4c63134a8518894
SelinaYu33 <zhishay1@uci.edu>
2025-02-13 22:31:56 +0800

    fix extensions filter

--- scraper.py
+++ scraper.py
@@ -555,8 +555,7 @@ def is_valid(url):
         # File extension filtering
         invalid_extensions = {
             # Documents
-            'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'txt', 'rtf', 'odc'
-            'odt', 'ods', 'odp', 'tex', 'ps', 'eps', 'ppsx',
+            'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'txt', 'rtf', 'odc', 'odt', 'ods', 'odp', 'tex', 'ps', 'eps',
             # Images
             'jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'ico', 'svg', 'webp', 'heic', 'heif', 'hevc', 'avif', 'img',
             # Audio/Video
